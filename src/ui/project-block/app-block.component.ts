238dff2cd356c40bd52fd6e48388fcb7ffe07625
Joinsider <johannes@fampopprol.me>
2024-10-14 21:49:25 +0200

    Initial Commit

---
+++ src/ui/project-block/app-block.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-block',
+  template: `<p>Hello</p>`,
+  styles: `p { color: red; }`,
+  standalone: true,
+})
+export class AppBlockComponent {
+}
