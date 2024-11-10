f8cca15ba692eb9b42a833070b84f4c856e3702d
Joinsider <johannes@fampopprol.me>
2024-11-10 17:15:51 +0100

    Add Contact Form

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Johannespopp.social
 
-My personal webpage built with angular. It shows basic information about myself and includes social links and links for my github projects. 
+My personal webpage built with angular. It shows basic information about myself and includes social links and links for my github projects.
+
+View the webpage [here](https://johannespopp.social).
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import {GithubProjectsComponent} from './github/component/github.component';
 import {ProfileComponent} from './profile/profile.component';
 import {SocialsComponent} from './socials/component/socials.component';
+import {ContactComponent} from './contact/component/contact.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, GithubProjectsComponent, ProfileComponent, SocialsComponent],
+  imports: [RouterOutlet, GithubProjectsComponent, ProfileComponent, SocialsComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
